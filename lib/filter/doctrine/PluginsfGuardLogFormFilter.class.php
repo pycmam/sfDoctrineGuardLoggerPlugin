de3c14c788cff630b52dc9884c46b5f624d03392
pycmam <pycmam@gmail.com>
2011-11-04 16:12:06 +0600

    Причесал модуль

--- lib/filter/doctrine/PluginsfGuardLogFormFilter.class.php
+++ lib/filter/doctrine/PluginsfGuardLogFormFilter.class.php
@@ -2,11 +2,6 @@
 
 /**
  * PluginsfGuardLog form.
- *
- * @package    ##PROJECT_NAME##
- * @subpackage filter
- * @author     ##AUTHOR_NAME##
- * @version    SVN: $Id: sfDoctrineFormFilterPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 abstract class PluginsfGuardLogFormFilter extends BasesfGuardLogFormFilter
 {
